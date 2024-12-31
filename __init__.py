0cd6a50de3e7e8c4fd2a710cfa0020eb66e81ad9
soukaina-17 <soukaina.ljaouhari@gmail.com>
2024-12-31 02:20:21 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,8 +8,10 @@ app = Flask(__name__)
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
-                                                                                                                                       
-
+  
+  @app.route('/')
+def hello_world():
+    return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"                                                                                                                                     
 
 @app.route('/exercices/') #com
 def exercices():
