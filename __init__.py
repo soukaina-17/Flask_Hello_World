2fd792f46ae160c97baa4f110ed4ec82b5e6118a
soukaina-17 <soukaina.ljaouhari@gmail.com>
2024-12-31 02:25:24 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,10 +5,6 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)
 
-@app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
-  
   @app.route('/')
 def hello_world():
     return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"                                                                                                                                     
