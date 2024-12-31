bd04dfbbd9c316f75888762c08c43a13362dd16b
soukaina-17 <soukaina.ljaouhari@gmail.com>
2024-12-31 02:45:07 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,13 +3,13 @@ from flask import render_template
 from flask import json
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)
-
-  @app.route('/')
+app = Flask(__name__)                                                                                                                  
+                                                                                                                                       
+@app.route('/')
 def hello_world():
-    return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"                                                                                                                                   
+    return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
 
-@app.route('/exercices/') #com
+@app.route('/exercices/')
 def exercices():
     return render_template('exercices.html')
                                                                                                                
