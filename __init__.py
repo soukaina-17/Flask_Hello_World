c459ff6848d14fa2ce9e6b295856b0dabe9a3517
soukaina-17 <soukaina.ljaouhari@gmail.com>
2025-01-02 21:45:44 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,6 +16,10 @@ def exercices():
 @app.route('/contact/')
 def MaPremierAPI():
   return render_template("contact.html")
+
+@app.route('/calcul_carre/<int:val_user>')
+def carre(val_user):
+    return "<h2>Le carré de votre valeur est : </h2>" + str(val_user * val_user)
                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)
